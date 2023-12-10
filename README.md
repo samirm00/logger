6787a23ab56905e3b2e4da74024119bf87583566
samirm00 <sammoham.be@gmail.com>
2023-12-10 19:13:40 +0100

    add class DateTime

--- README.md
+++ README.md
@@ -1,6 +1,39 @@
-# Get date and time now
+# DateTime
 
-```js
-import now from 'get-date-time-now';
-const current = now();
+The `DateTime` class is a JavaScript class that provides methods for handling date and time.
+
+## Installation
+
+```bash
+npm i get-date-time-now
+```
+
+## Usage
+
+```javascript
+// Import the DateTime class
+import DateTime from 'get-date-time-now';
+
+// Create an instance of the DateTime class
+const dateTime = new DateTime();
+
+// Get the current day
+const currentDay = dateTime.getCurrentDay(); // ex: Sunday
+
+// Get the current month
+const currentMonth = dateTime.getCurrentMonth(); // ex: July
+
+// Get the current formatted date and time
+const formattedDateTime = dateTime.getFormattedDateTime(); // ex: 2023/10/10 12:33:00
+
+// Get yesterday's date
+const yesterday = dateTime.getYesterday(); // ex : 2023/10/09
+
+// Get tomorrow's date
+const tomorrow = dateTime.getTomorrow(); //ex :  2023/10/11
+
+// Format a specific date and time
+const specificDate = new Date('2023-01-01T12:30:00');
+const formattedDate = dateTime.formatDate(specificDate); // ex: 2023/10/10
+const formattedTime = dateTime.formatTime(specificDate); // ex: 12:33:00
 ```
---
+++ date-time.js
@@ -0,0 +1,83 @@
+class DateTime {
+    constructor() {
+        this.days = [
+            'Sunday',
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday',
+        ];
+        this.months = [
+            'January',
+            'February',
+            'March',
+            'April',
+            'May',
+            'June',
+            'July',
+            'August',
+            'September',
+            'October',
+            'November',
+            'December',
+        ];
+    }
+
+    getCurrentDay = () => {
+        const dayIndex = new Date().getDay();
+        return this.days[dayIndex];
+    };
+
+    getCurrentMonth = () => {
+        const monthIndex = new Date().getMonth();
+        return this.months[monthIndex];
+    };
+
+    getFormattedDateTime = () => {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = this.formatNumber(now.getMonth() + 1);
+        const day = this.formatNumber(now.getDate());
+        const hours = this.formatNumber(now.getHours());
+        const minutes = this.formatNumber(now.getMinutes());
+        const seconds = this.formatNumber(now.getSeconds());
+
+        return `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
+    };
+
+    formatNumber = (num) => {
+        return num < 10 ? '0' + num : num;
+    };
+
+    getYesterday = () => {
+        const yesterday = new Date();
+        yesterday.setDate(yesterday.getDate() - 1);
+        return this.formatDate(yesterday);
+    };
+
+    getTomorrow = () => {
+        const tomorrow = new Date();
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        return this.formatDate(tomorrow);
+    };
+
+    formatDate = (date) => {
+        const year = date.getFullYear();
+        const month = this.formatNumber(date.getMonth() + 1);
+        const day = this.formatNumber(date.getDate());
+
+        return `${year}/${month}/${day}`;
+    };
+
+    formatTime = (date) => {
+        const hours = this.formatNumber(date.getHours());
+        const minutes = this.formatNumber(date.getMinutes());
+        const seconds = this.formatNumber(date.getSeconds());
+
+        return `${hours}:${minutes}:${seconds}`;
+    };
+}
+
+export default DateTime;
